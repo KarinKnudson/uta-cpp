75ba5a5d2d010b9b6f0d9325a5534b5192eb24b2
KarinKnudson <shorty@karin-knudsons-macbook-pro.local>
2012-09-16 11:33:22 -0500

    This is vim?  Someday, Hendrik will have to explain what he likes about this (ORhow to become competent with this...

--- project_euler/Problem1KCK.cpp
+++ project_euler/Problem1KCK.cpp
@@ -1,5 +1,5 @@
 /*
- *  Problem1KCK.cpp
+ *  Problem1KCK.h
  *  
  *
  *  Created by Shorty Knudson on 9/16/12.
@@ -7,5 +7,29 @@
  *
  */
 
-#include "Problem1KCK.h"
+#include <iostream>
+using namespace std;
 
+int main()
+{
+	int multiples = 0;
+	int x = 1;
+	
+	//sum up all the numbers that are multiples of three but not five, and are less than 1000
+	while (3*x <1000 ) {
+		if ((3*x) % 5 !=0){
+			//cout << 3*x<<"\n";
+			multiples = multiples + 3*x;
+		}
+		x++;
+	}
+	
+	//add to the previous sum all the numbers less than 1000 that are multiples of five
+	x = 1;
+	while (5*x < 1000) {
+		multiples = multiples + 5*x;
+		x++;
+	}
+	cout << multiples<<"\n";
+	return 0;
+}
